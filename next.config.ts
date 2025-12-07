d591fc8e14254df7afd180248e3206e55664ed6e
somesnakeguy <somesnakeguy@protonmail.com>
2025-12-07 00:55:18 -0500

    Updating basepath

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ const withBuilderDevTools = require("@builder.io/dev-tools/next")();
 /** @type {import('next').NextConfig} */
 const nextConfig = withBuilderDevTools({
   output: "export", // Critical for GitHub Pages
-  basePath: "",
+  basePath: "/coomer_mobile_pages",
   images: {
     unoptimized: true, // Required for static export
   },
